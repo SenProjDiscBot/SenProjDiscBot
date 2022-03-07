b22f2a0ec3c73d0f7dc8ff7d877860263b3f9e17
DannB11 <89940126+DannB11@users.noreply.github.com>
2022-03-07 16:12:09 -0700

    Update timeclock.py

--- timeclock.py
+++ timeclock.py
@@ -1,116 +1,115 @@
 from datetime import datetime
 from datetime import time
+from discord import DMChannel
 from discord.ext import commands
+import pymongo
+from connect_to_db import connect_to_db
 
-class timeclock(commands.Cog):
+class register(commands.Cog):
 
   def __init__(self,client):
     self.client = client
-
-  @commands.command(name='in')
-  async def clockIn(self, ctx):
-    #build file name from Member class
-    name = str(ctx.author)
-    file_name = name + ".txt"
-
-    #try to create file
-    try:
-     file = open(file_name, 'x')
-     file.close()
-    except:
-      print("File already exists")
-
-    #open file in read/write mode
-    file = open(file_name, 'r+')
-    #parse lines
-    var = file.read().split("\n")
-    #check if file is empty
-    if var[0] != "":
-      #check if already clocked in
-      last_line = var[len(var) - 2]
-      words = last_line.split(" ")
-      if words[0] == "in":
-        await ctx.send("You are already clocked in")
-        file.close()
-        return
-    #write clock in date and time to file
-    now = datetime.now()
-    record = "in " + str(now.strftime("%d/%m/%Y %H:%M") + "\n")
-    file.write(record)
-    file.flush()
-    file.close()
-    await ctx.send("You are now clocked in starting at: " + str(now.strftime("%d/%m/%Y %H:%M")))
+    # connect to mongo db
+    print("Register connecting to mongoDB....")
+    self.db = connect_to_db()
+    print("Register connected to database!")
 
 
-  @commands.command(name='out')
-  async def clockOut(self, ctx):
-    name = str(ctx.author)
-    file_name = name + ".txt"
-    try:
-     file = open(file_name, 'x')
-     file.close()
-    except:
-      print("File already exists")
-    file = open(file_name, 'r+')
-    var = file.read().split("\n")
 
-    time = 0;
-    now = datetime.now()
-    if var[0] != "":
-      last_line = var[len(var) - 2]
+  async def guild_null(self, ctx):
+    if ctx.guild == None:
+      await ctx.send("This command does not work in direct messages!")
+      return True
+    return False
 
-      words = last_line.split(" ")
 
-      if words[0] == "out":
-        await ctx.send("You are not clocked in yet")
-        file.close()
-        return
-      if words[0] == "in":
-        in_time = words[2].split(":")
-        hours = int(in_time[0])
-        minutes = int(in_time[1])
-        current_time = now.strftime("%H:%M").split(":")
-        c_hours = int(current_time[0])
-        c_minutes = int(current_time[1])
-        time = c_hours - hours
-        time = time + ((c_minutes - minutes)/60)
-    record = "out " + str(now.strftime("%d/%m/%Y %H:%M") + "\n")
-    file.write(record)
-    file.flush()
-    file.close()
-    await ctx.send("You are clocked out.\nYou worked " + str(time) + " hours.")
+  @commands.command(name='register')
+  async def register(self, ctx):
+    # check if command is not in direct messages
+    if await self.guild_null(ctx):
+      return
+    #get user discord id
+    discord_id = str(ctx.author)
+  
+    #connect to mongoDB
+    records = self.db.get_employee_records(ctx)
 
-  @commands.command(name='hours')
-  async def checkHours(self, ctx):
-    name = str(ctx.author)
-    file_name = name + ".txt"
-
-    file = open(file_name, 'r')
-    var = file.read().split("\n")
-    ins = 0
-    outs = 0
-    in_count = 0
-    out_count = 0
-    last_in_date = ""
-    for line in var:
-      if line == "":
-        continue
-      words = line.split(" ")
-      hours = words[2].split(":")[0]
-      minutes = words[2].split(":")[1]
-      if words[0] == "in":
-        ins = ins + int(hours) + (int(minutes)/60)
-        in_count += 1
-      elif words[0] == "out":
-        outs = outs + int(hours) + (int(minutes)/ 60)
-        out_count+= 1
-    time = outs - ins
-    file.close()
-    if in_count != out_count:
-      await ctx.send("Please clock out before checking total hours worked.")
+    #check if discord user is already in the database
+    if await self.db.check_active(ctx):
+      await ctx.send("You are already in this database.")
       return
 
-    await ctx.send("You have worked a total of " + str(time) + " hours.")
+    #open a dm channel with user
+    dm = await ctx.author.create_dm()
+
+    #helper function that checks if a message is in the authors dm channel
+    def check(msg):
+      return msg.author == ctx.author and msg.channel == dm
+
+    #get first name
+    await dm.send("First Name?")
+    fn = await self.client.wait_for("message", check=check)
+    first_name = fn.content
+
+    #get last name
+    await dm.send("Last Name?")
+    ln = await self.client.wait_for("message", check=check)
+    last_name = ln.content
+
+    #create employee dict
+    new_employee = {
+        'discord_id' : discord_id,
+        'name_first' : first_name,
+        'name_last' : last_name
+    }
+    #store employee in the database
+    records.insert_one(new_employee)
+
+    #post verfication in discord channel
+    await ctx.send(first_name + " " + last_name + " created in the database.")
+
+
+
+  @commands.command(name='edit')
+  async def edit(self, ctx):
+    # check if command is not in direct messages
+    if await self.guild_null(ctx):
+      return
+    #get user discord id
+    discord_id = str(ctx.author)
+
+    #get mongoDB table
+    records = self.db.get_employee_records(ctx)
+    #check if discord user is already in the database
+    if not await self.db.check_active(ctx):
+      return
+
+    #open a dm channel with user
+    dm = await ctx.author.create_dm()
+
+    #helper function that checks if a message is in the authors dm channel
+    def check(msg):
+      return msg.author == ctx.author and msg.channel == dm
+    old = records.find_one({'discord_id' : discord_id})
+    old_first = old['name_first']
+    old_last = old['name_last']
+
+    #get first name
+    await dm.send("Current: " + old_first + "\nFirst Name?")
+    fn = await self.client.wait_for("message", check=check)
+    first_name = fn.content
+
+    #get last name
+    await dm.send("Current: " + old_last + "\nLast Name?")
+    ln = await self.client.wait_for("message", check=check)
+    last_name = ln.content
+
+    #store employee in the database
+    records.update_one({'discord_id' : discord_id} , {"$set":{'name_first' : first_name, 'name_last' : last_name}})
+
+    #post verfication in discord channel
+    await dm.send("You have updated your information.")
+
 
 def setup(client):
-  client.add_cog(timeclock(client))
+  client.add_cog(register(client))
