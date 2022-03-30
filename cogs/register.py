edec7912c88b89eb0f0dd56c17a3a98aca9abe84
DannB11 <89940126+DannB11@users.noreply.github.com>
2022-03-29 22:36:02 -0600

    Update register.py

--- cogs/register.py
+++ cogs/register.py
@@ -24,7 +24,7 @@ class register(commands.Cog):
     async def register(self, ctx):
         # check if command is not in direct messages
         if await self.guild_null(ctx):
-           return
+            return
         # get user discord id
         discord_id = str(ctx.author)
         guild_id = str(ctx.guild.id)
@@ -63,7 +63,9 @@ class register(commands.Cog):
                           SelectOption(label="MST	Mountain Standard Time GMT-7:00", value='US/Mountain'),
                           SelectOption(label="CST	Central Standard Time	GMT-6:00", value='US/Central'),
                           SelectOption(label="EST	Eastern Standard Time	GMT-5:00", value='US/Eastern'),
-                          SelectOption(label="PRT	Puerto Rico and US Virgin Islands Time GMT-4:00", value='America/Puerto_Rico'),
+                          SelectOption(
+                              label="PRT	Puerto Rico and US Virgin Islands Time GMT-4:00",
+                              value='America/Puerto_Rico'),
                           SelectOption(label="AGT	Argentina Standard Time	GMT-3:00", value='America/Argentina/Ushuaia'),
                           SelectOption(label="UTC	Universal Coordinated Time GMT+0:00", value='UTC'),
                           SelectOption(label="ECT	European Central Time	GMT+1:00", value='Europe/Rome'),
@@ -80,9 +82,10 @@ class register(commands.Cog):
                           SelectOption(label="NST	New Zealand Standard Time	GMT+12:00", value='Pacific/Fiji')
                 ],
                 custom_id='timezone'
-                )
-            ])
-        timezone_interaction = await self.client.wait_for("select_option", check=lambda i: i.custom_id == "timezone" and i.user == ctx.author)
+            )
+        ])
+        timezone_interaction = await self.client.wait_for(
+            "select_option", check=lambda i: i.custom_id == "timezone" and i.user == ctx.author)
         timezone = timezone_interaction.values[0]
         await self.clear_last_msg(dm)
         member_id = ctx.author.id
@@ -117,7 +120,8 @@ class register(commands.Cog):
                   )
             ])
 
-            promote_interaction = await self.client.wait_for("select_option", check=lambda i: i.custom_id == "promote" and i.user == ctx.author)
+            promote_interaction = await self.client.wait_for(
+                "select_option", check=lambda i: i.custom_id == "promote" and i.user == ctx.author)
             promote = str(promote_interaction.values[0])
 
             await self.clear_last_msg(dm)
@@ -155,7 +159,8 @@ class register(commands.Cog):
                             default: PermissionOverwrite(read_messages=False),
                             manager_role: PermissionOverwrite(read_messages=True)
                         }
-                        channel = await ctx.guild.create_text_channel(name='timeclock-manager-log', overwrites=overwrites)
+                        channel = await ctx.guild.create_text_channel(
+                            name='timeclock-manager-log', overwrites=overwrites)
 
         else:
             await dm.send("You do not have permission to access this command. (peasant)")
@@ -179,26 +184,27 @@ class register(commands.Cog):
                 ops.append(SelectOption(label=label, value=str(emp['discord_id'])))
 
             ops.append(SelectOption(label='Cancel', value='end'))
-            await dm.send("Who would you like to demote?",components =[
-            Select(
-                placeholder="Select a manager for demotion",
-                options=ops,
-                custom_id='demote'
+            await dm.send("Who would you like to demote?", components=[
+                Select(
+                    placeholder="Select a manager for demotion",
+                    options=ops,
+                    custom_id='demote'
                 )
             ])
 
-            demote_interaction = await self.client.wait_for("select_option", check=lambda i: i.custom_id == "demote" and i.user == ctx.author)
+            demote_interaction = await self.client.wait_for(
+                "select_option", check=lambda i: i.custom_id == "demote" and i.user == ctx.author)
             demote = str(demote_interaction.values[0])
             await self.clear_last_msg(dm)
             if demote == "end":
-                await dm.send("Demotion canceled.") 
+                await dm.send("Demotion canceled.")
 
             else:
                 self.db.demote_manager(demote, guild_id)
                 manager_role = get(ctx.guild.roles, name="Manager")
                 slice = records.find_one({'discord_id': demote})
                 member_id = slice['member_id']
-                user =  get(ctx.guild.members, id=member_id)
+                user = get(ctx.guild.members, id=member_id)
                 await user.remove_roles(manager_role, atomic=True)
                 await dm.send("Demotion complete.")
 
@@ -221,12 +227,14 @@ class register(commands.Cog):
 
         # open a dm channel with user
         dm = await ctx.author.create_dm()
-        # ######## v ################ v REMOVE EVENTUALLY ##################### v ##########################################################################################################################################
+        # ######## v ################ v REMOVE EVENTUALLY ##################### v
         self.db.update_all_manager_values(guild_id)
-        # ######### ^ ################## REMOVE EVENTUALLY ####################### ^ ##################################################################################################################################################################
+        # ######### ^ ################## REMOVE EVENTUALLY ####################### ^
         # helper function that checks if a message is in the authors dm channel
+
         def check(msg):
             return msg.author == ctx.author and msg.channel == dm
+
         old = records.find_one({'discord_id': discord_id})
         old_first = old['name_first']
         old_last = old['name_last']
@@ -242,7 +250,7 @@ class register(commands.Cog):
         last_name = ln.content
 
         # get timezone
-        await dm.send("What is your timezone?", components = [
+        await dm.send("What is your timezone?", components=[
             Select(
                 placeholder="Timezone",
                 options=[
@@ -253,7 +261,10 @@ class register(commands.Cog):
                           SelectOption(label="MST	Mountain Standard Time	GMT-7:00", value='US/Mountain'),
                           SelectOption(label="CST	Central Standard Time	GMT-6:00", value='US/Central'),
                           SelectOption(label="EST	Eastern Standard Time	GMT-5:00", value='US/Eastern'),
-                          SelectOption(label="PRT	Puerto Rico and US Virgin Islands Time	GMT-4:00", value='America/Puerto_Rico'),
+                          SelectOption(
+                              label="PRT	Puerto Rico and US Virgin Islands Time	GMT-4:00",
+                              value='America/Puerto_Rico'
+                          ),
                           SelectOption(label="AGT	Argentina Standard Time	GMT-3:00", value='America/Argentina/Ushuaia'),
                           SelectOption(label="UTC	Universal Coordinated Time	GMT+0:00", value='UTC'),
                           SelectOption(label="ECT	European Central Time	GMT+1:00", value='Europe/Rome'),
@@ -272,12 +283,17 @@ class register(commands.Cog):
                 custom_id='timezone'
                 )
         ])
-        timezone_interaction = await self.client.wait_for("select_option", check=lambda i: i.custom_id == "timezone" and i.user == ctx.author)
+        timezone_interaction = await self.client.wait_for(
+            "select_option", check=lambda i: i.custom_id == "timezone" and i.user == ctx.author)
         timezone = timezone_interaction.values[0]
         await self.clear_last_msg(dm)
 
         # store employee in the database
-        records.update_one({'discord_id': discord_id} , {"$set": {'member_id': ctx.author.id, 'name_first': first_name, 'name_last': last_name, 'timezone': timezone}})
+        records.update_one(
+            {'discord_id': discord_id} ,
+            {"$set": {'member_id': ctx.author.id,
+            'name_first': first_name, 'name_last': last_name, 'timezone': timezone}}
+          )
 
         # post verfication in discord channel
         await dm.send("You have updated your information.")
@@ -302,8 +318,8 @@ class register(commands.Cog):
             default = ctx.guild.default_role
 
             overwrites = {
-            default: PermissionOverwrite(read_messages=False),
-            manager_role: PermissionOverwrite(read_messages=True)
+              default: PermissionOverwrite(read_messages=False),
+              manager_role: PermissionOverwrite(read_messages=True)
             }
             channel = await ctx.guild.create_text_channel(name='timeclock-manager-log', overwrites=overwrites)
 
