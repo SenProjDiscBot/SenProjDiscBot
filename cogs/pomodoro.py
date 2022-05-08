496cc3cfdae5f44a297cbffabf00af9b53f02339
sbowma15 <70663678+sbowma15@users.noreply.github.com>
2022-05-08 15:38:20 -0600

    Add files via upload
    Got rid of "Quitpomodoro" command and gutted and reformulated the "pomodoro" command function to allow user to opt in and out at each time interval WITHOUT crashing the bot this time. Its been tested with all possible inputs and works without fail as far as I can see.

--- cogs/pomodoro.py
+++ cogs/pomodoro.py
@@ -32,69 +32,80 @@ class pomodoro(commands.Cog):
 
         converted_25 = int(25) * 60
         converted_5 = int(5) * 60
+
         i = 0
-        while i < 16:
-            await asyncio.sleep(converted_25)
-            qrembed2 = discord.Embed(
-                description="please take a 5 minute break", color=0x000FF
-            )
-            await ctx.send(embed=qrembed2)
-            await asyncio.sleep(converted_5)
-            qrembed3 = discord.Embed(
-                description="Your 5 minute pomodoro break is over, please choose another task to focus on for 25 minutes",
-                color=0x000FF,
-            )
-            await ctx.send(embed=qrembed3)
-            i = i + 1
-
-    @commands.command(name="quitpomodoro")
-    async def quitpomodoro(self, ctx):
-
-        if await self.guild_null(ctx):
-            return
 
         def check(msg):
             return msg.author == ctx.author and msg.channel
 
-        qrembed1 = discord.Embed(
-            description="Would you like to end your pomodoro session?", color=0x000FF
-        )
-        await ctx.send(embed=qrembed1)
-        qr = await self.client.wait_for("message", check=check)
+        while i < 16:
 
-        # await ctx.send(embed=qrembed, components=[
-        #  Button(label="Yes", style="3", custom_id="yes"),
-        #  Button(label="No", style="4", custom_id="no")])
-        # try:
-        # qr = await self.client.wait_for("button_click", timeout=300, check=lambda i: i.custom_id == "yes" or "no")
-        qrembed = discord.Embed(
-            description="You have now exited the Pomodoro Timer!", color=0x000FF
-        )
-        qrembed5 = discord.Embed(
-            description="You are still using the Pomodoro Timer!", color=0x000FF
-        )
-        qrembed6 = discord.Embed(
-            description="you must enter: ' YES '... otherwise you are still using the Pomodoro timer!",
-            color=0x000FF,
-        )
+            qrembed = discord.Embed(
+                description="You have now exited the Pomodoro Timer!", color=0x000FF
+            )
+            qrembed5 = discord.Embed(
+                description="You are still using the Pomodoro Timer!", color=0x000FF
+            )
+            qrembed6 = discord.Embed(
+                description="you must enter: ' YES '... otherwise you are still using the Pomodoro timer!",
+                color=0x000FF,
+            )
+            await asyncio.sleep(converted_25)
+            qrembed2 = discord.Embed(
+                description="please take a 5 minute break, or would you like to end Pomodoro Session? (YES/NO)",
+                color=0x000FF,
+            )
+            await ctx.send(embed=qrembed2)
+            qr = await self.client.wait_for("message", check=check)
+            if str(qr.content) == "yes":
+                await ctx.send(embed=qrembed)
+                i = 16
+                break
+            elif str(qr.content) == "Yes":
+                await ctx.send(embed=qrembed)
+                i = 16
+                break
+            elif str(qr.content) == "YES":
+                await ctx.send(embed=qrembed)
+                i = 16
+                break
+            elif qr.content == "NO":
+                await ctx.send(embed=qrembed5)
+            elif qr.content == "no":
+                await ctx.send(embed=qrembed5)
+            elif qr.content == "No":
+                await ctx.send(embed=qrembed5)
+            else:
+                await ctx.send(embed=qrembed6)
 
-        if str(qr.content) == "yes":
-            await ctx.send(embed=qrembed)
-            quit()
-        elif str(qr.content) == "Yes":
-            await ctx.send(embed=qrembed)
-            quit()
-        elif str(qr.content) == "YES":
-            await ctx.send(embed=qrembed)
-            quit()
-        elif qr.content == "NO":
-            await ctx.send(embed=qrembed5)
-        elif qr.content == "no":
-            await ctx.send(embed=qrembed5)
-        elif qr.content == "No":
-            await ctx.send(embed=qrembed5)
-        else:
-            await ctx.send(embed=qrembed6)
+            await asyncio.sleep(converted_5)
+            qrembed3 = discord.Embed(
+                description="Your 5 minute pomodoro break is over, please choose another task to focus on for 25 minutes, or would you like to end your pomodoro session?(YES/NO)",
+                color=0x000FF,
+            )
+            await ctx.send(embed=qrembed3)
+            qr = await self.client.wait_for("message", check=check)
+            if str(qr.content) == "yes":
+                await ctx.send(embed=qrembed)
+                i = 16
+                break
+            elif str(qr.content) == "Yes":
+                await ctx.send(embed=qrembed)
+                i = 16
+                break
+            elif str(qr.content) == "YES":
+                await ctx.send(embed=qrembed)
+                i = 16
+                break
+            elif qr.content == "NO":
+                await ctx.send(embed=qrembed5)
+            elif qr.content == "no":
+                await ctx.send(embed=qrembed5)
+            elif qr.content == "No":
+                await ctx.send(embed=qrembed5)
+            else:
+                await ctx.send(embed=qrembed6)
+            i = i + 1
 
         if await self.guild_null(ctx):
             return
